98c4ecb1a708a882827c8a6d4c049098fbe000db
SerisKey <zihlingchen@gmail.com>
2024-08-11 23:10:06 +0800

    todo api

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-2024-week02/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
